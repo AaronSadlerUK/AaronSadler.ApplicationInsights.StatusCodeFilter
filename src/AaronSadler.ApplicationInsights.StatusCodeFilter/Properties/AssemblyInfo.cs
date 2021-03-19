7a3290083cdeb44dc0309216dededaedec089f51
Aaron Sadler <aaron@bdci.co.uk>
2021-03-19 10:45:04 +0000

    Initial Commit

--- src/AaronSadler.ApplicationInsights.StatusCodeFilter/Properties/AssemblyInfo.cs
+++ src/AaronSadler.ApplicationInsights.StatusCodeFilter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AaronSadler.ApplicationInsights.StatusCodeFilter")]
-[assembly: AssemblyDescription("Staus code filter for Application Insights to record as success instead of failure")]
+[assembly: AssemblyDescription("Staus code filter for Application Insights to record as success instead of failure for Umbraco 8")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AaronSadlerUK")]
 [assembly: AssemblyProduct("AaronSadler.ApplicationInsights.StatusCodeFilter")]
